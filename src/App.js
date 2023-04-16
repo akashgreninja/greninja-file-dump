805e3ba3b97e2ce42e27dd4a23447fe1b6566535
akashgreninja <akashuhulekal@gmail.com>
2023-04-17 00:46:13 +0530

    added all changes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 import "./App.css";
-import { getStorage, ref, uploadBytes,getDownloadURL } from "firebase/storage";
+import Confetti from "react-confetti";
+
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 function App() {
   const [image, setimage] = useState("");
+  const [loading, setloading] = useState(false);
+  const [done, setdone] = useState(false);
   const handleFile = (e) => {
- 
     if (e.target.files && e.target.files.length === 1) {
       setimage(e.target.files[0]);
     }
@@ -15,37 +18,84 @@ function App() {
     try {
       const storage = getStorage();
       const storageRef = ref(storage, `Akash/${image.name}`);
-      // setloading(true);
+      setloading(true);
 
       uploadBytes(storageRef, image).then((snapshot) => {
         getDownloadURL(storageRef).then((url) => {
           setimage(url);
-
         });
-        console.log("here")
+        console.log("here");
 
-        // setloading(false);
+        setloading(false);
+        setdone(true);
+        setTimeout(() => {
+          setdone(false);
+        }, 3000);
       });
     } catch (e) {
       console.log(e);
     }
   };
+
   return (
-    <div className="App">
-      <form>
-        <input
-          type="file"
-          onChange={handleFile}
-          accept=".jpg,.png,.jpeg,.gif,.svg,.pdf,.doc,.docx,.xls,.xlsx,.ppt,.pptx,.txt,.csv,.zip,.rar,.7z,.tar,.gz,.bz2,.mp3,.mp4,.wav,.avi,.mov,.wmv,.flv,.mkv,.3gp,.webm,.m4v,.mpg,.mpeg,.swf,.vob,.ogg,.ogv,.m4a,.wma,.aac,.f"
-          name="photo"
-          className="form-control"
-          id="photo"
-          minLength={1}
-          required
-        />
-      </form>
-      <button onClick={CreateUpload}>Upload</button>
-    </div>
+    <center>
+      <div className="App">
+        <h1>GRENINJA FILE DUMP</h1>
+        <form>
+          <input
+            type="file"
+            webkitdirectory
+            onChange={handleFile}
+            accept="*/*"
+            name="photo"
+            className="form-control"
+            id="photo"
+            minLength={1}
+            required
+          />
+        </form>
+        <button onClick={CreateUpload}>Upload</button>
+        {loading === true ? (
+          <div
+            aria-label="Orange and tan hamster running in a metal wheel"
+            role="img"
+            class="wheel-and-hamster"
+          >
+            <div class="wheel"></div>
+            <div class="hamster">
+              <div class="hamster__body">
+                <div class="hamster__head">
+                  <div class="hamster__ear"></div>
+                  <div class="hamster__eye"></div>
+                  <div class="hamster__nose"></div>
+                </div>
+                <div class="hamster__limb hamster__limb--fr"></div>
+                <div class="hamster__limb hamster__limb--fl"></div>
+                <div class="hamster__limb hamster__limb--br"></div>
+                <div class="hamster__limb hamster__limb--bl"></div>
+                <div class="hamster__tail"></div>
+              </div>
+            </div>
+            <div class="spoke"></div>
+          </div>
+        ) : null}
+        {done === true ? (
+          <>
+            <div class="upload-message">
+              <p>Text successfully uploaded!</p>
+            </div>
+            <Confetti
+              width={window.innerWidth}
+              height={window.innerHeight}
+              numberOfPieces={200}
+              recycle={false}
+              gravity={0.5}
+              colors={["#ff0000", "#00ff00", "#0000ff"]}
+            />
+          </>
+        ) : null}
+      </div>
+    </center>
   );
 }
 
